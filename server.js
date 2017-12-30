be8ad0d031fea5e6c42ea33ccfef4a9f5af22b53
amin jamal <aminjamal10@gmail.com>
2017-12-30 18:21:11 +0330

    clean server.js

--- server.js
+++ server.js
@@ -4,32 +4,10 @@ var express = require('express');
 var mongodb = require('mongodb').MongoClient;
 var app = express()
 
-class test{
-    constructor(name,lname,age){
-        this.name = name;
-        this.lname = lname;
-        this.age = age;
-
-    }
-     getname() {
-         return name;
-        
-    }
-   }
-
-
 app.get('/', (req, res) => {
 
-    var array = []
-    array.unshift('amin')
-    array.unshift('reza')
-
-    var amin  = new test("amin","jamal","22");
-    console.log(amin);
-    res.send(array);
     
 });
-
 
 
 app.listen(9000, () => {
